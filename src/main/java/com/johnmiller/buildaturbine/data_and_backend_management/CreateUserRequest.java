221a19f23f026277b9e5dff8f592b4753c3569a9
John Miller <johnmiller@macbookpro.attlocal.net>
2026-01-21 23:01:46 -0800

    refactoring codebase into microservices

--- src/main/java/com/johnmiller/buildaturbine/data_and_backend_management/CreateUserRequest.java
+++ src/main/java/com/johnmiller/buildaturbine/data_and_backend_management/CreateUserRequest.java
@@ -1,5 +1,5 @@
 package com.johnmiller.buildaturbine.data_and_backend_management;
-import jakarta.validation.constraints.NotBlank;;
+import jakarta.validation.constraints.NotBlank;
 
 public record CreateUserRequest(
     @NotBlank String username,
