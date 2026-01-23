e2c274416f23cf246a021bd4904276fa1850578f
John Miller <johnmiller@macbookpro.attlocal.net>
2026-01-23 03:22:18 -0800

    commiting deep learning

--- src/main/java/com/johnmiller/buildaturbine/driver_code.java
+++ src/main/java/com/johnmiller/buildaturbine/driver_code.java
@@ -7,8 +7,5 @@ public class driver_code {
 
 	public static void main(String[] args) {
 		SpringApplication.run(driver_code.class, args);
-		// String variable = System.getenv().get("BASE_URL_ALLOW_FRONTEND");
-		// System.out.println(variable);
-		// System.out.println("HIII");
 	}
 }
